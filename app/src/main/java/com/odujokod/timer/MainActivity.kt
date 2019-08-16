d9616fac42af22fa938e9b6b883ba7c91733a6f6
Oluwadamilola Odujoko <odujoko.dml30@gmail.com>
2019-08-16 10:53:53 +0100

    Modify UI

--- app/src/main/java/com/odujokod/timer/MainActivity.kt
+++ app/src/main/java/com/odujokod/timer/MainActivity.kt
@@ -2,20 +2,42 @@ package com.odujokod.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var chronometer: Chronometer
+
+    private var running: Boolean = false
+    private var offset: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chronometer = findViewById(R.id.chronometer)
+        startButton = findViewById(R.id.btn_start)
+        stopButton = findViewById(R.id.btn_stop)
     }
 
     fun startTimer(view: View) {
-
+        if (!running) {
+            chronometer.base = SystemClock.elapsedRealtime() - offset
+            chronometer.start()
+            running = true
+        }
     }
 
     fun stopTimer(view: View) {
-
+        if (running) {
+            chronometer.stop()
+            offset = SystemClock.elapsedRealtime() - chronometer.base
+            running = false
+        }
     }
 }
