126e77828674fdd65c6f992257d98cea043ba1df
Oluwadamilola Odujoko <odujoko.dml30@gmail.com>
2019-08-16 10:01:13 +0100

    Implement template

--- app/src/main/java/com/odujokod/timer/MainActivity.kt
+++ app/src/main/java/com/odujokod/timer/MainActivity.kt
@@ -2,11 +2,20 @@ package com.odujokod.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startTimer(view: View) {
+
+    }
+
+    fun stopTimer(view: View) {
+
     }
 }
